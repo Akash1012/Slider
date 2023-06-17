70971e224d53e3791411da4e22d0a69262289cdb
akash <akash@akashs-MacBook-Air.local>
2023-06-17 16:44:42 +0530

    Slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { sliderImage } from "./data";
 
 function App() {
+  const [imageData, setImageData] = useState(sliderImage);
+  const [imageChange, setImageChange] = useState(1);
+
+  const nextImage = () => {
+    if (imageChange == imageData.length) {
+      setImageChange(1);
+      return;
+    }
+    setImageChange((prevImage) => prevImage + 1);
+  };
+
+  const prevImage = () => {
+    if (imageChange == 1) {
+      setImageChange(imageData.length);
+      return;
+    }
+    setImageChange((prevImage) => prevImage - 1);
+  };
+
+  useEffect(() => {
+    let interval;
+
+    interval = setInterval(() => {
+      nextImage();
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [imageChange]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div
+        style={{
+          position: "relative",
+          padding: "10px",
+        }}
+      >
+        {imageData.map((item) => {
+          const { name, pic, id } = item;
+          return (
+            <div
+              style={{
+                display: id == imageChange ? "block" : "none",
+              }}
+            >
+              <img
+                src={pic}
+                alt={name}
+                style={{
+                  width: "100%",
+                  height: "400px",
+                }}
+              />
+              <p
+                style={{
+                  textAlign: "center",
+                  position: "absolute",
+                  width: "100%",
+                  bottom: "11px",
+                }}
+              >
+                {name}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          bottom: "68%",
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <button
+          onClick={() => prevImage()}
+          style={{
+            margin: "10px",
+            marginLeft: "20px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Prev
+        </button>
+        <button
+          onClick={() => nextImage()}
+          style={{
+            margin: "10px",
+            marginLeft: "20px",
+          }}
+        >
+          Next
+        </button>
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        {imageData.map((item) => {
+          const { id } = item;
+          return (
+            <span
+              onClick={() => setImageChange(id)}
+              style={{
+                height: "15px",
+                display: "inline-block",
+                borderRadius: "50%",
+                width: "15px",
+                cursor: "pointer",
+                margin: "0 2px",
+                backgroundColor: id == imageChange ? "yellowgreen" : "green",
+              }}
+            ></span>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const sliderImage = [
+  {
+    id: 1,
+    name: "IMAGE_1",
+    pic: "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+  },
+  {
+    id: 2,
+    name: "IMAGE_2",
+    pic: "https://www.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej.png",
+  },
+  {
+    id: 3,
+    name: "IMAGE_3",
+    pic: "https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg",
+  },
+];
